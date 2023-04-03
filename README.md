ea250d026b68eb635acc5aff1fcc5c5f470eee1f
gensirovsky <61220518+gensirovsky@users.noreply.github.com>
2023-04-03 10:53:09 +0300

    new project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-I do it next time...
+##How to install project
+```
+git clone https://github.com/Kullpoint/superfoodies.git .
+git checkout -b branchName
+npm i
+```
+
+##How to run project
+
+Starts gulp watcher (for building sass files to css) and shopify dev server
+```
+npm run dev
+```
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const clean = require("gulp-clean-css");
+
+const sassFiles = [
+    //application
+    "src/styles/application.scss",
+    //sections
+    // "src/styles/sections/section-hero.scss",
+];
+
+gulp.task("dev", () => {
+    return gulp.src(sassFiles)
+        .pipe(sass())
+        .pipe(clean({ compatibility: "ie11" }))
+        .pipe(gulp.dest("assets"));
+});
+
+gulp.task("watch", () => {
+    gulp.watch("src/styles/**/*.scss", gulp.series("dev"));
+});
