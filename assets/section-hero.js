3f57e1f496ca245e7e5a788859e5c4f87481e2db
Dima <deekorde@gmail.com>
2023-04-18 10:27:25 +0300

    some changes

--- assets/section-hero.js
+++ assets/section-hero.js
@@ -1,30 +1,14 @@
-import Swiper from './swiper-bundle.js';
-
-const slider = () => {
-    const container = document.getElementById("heroSliderContainer");
-
-    new Swiper('#hero-slider', {
-        speed: 1200,
-        loop: true,
-        allowTouchMove: true,
-        autoplay: true,
-        delay: 5000,
-        lidesPerView: 1,
-        effect: 'cube',
-        runCallbacksOnInit: true,
-        cubeEffect: {       
-            slideShadows: false,
-        },
-        navigation: {
-            prevEl: '[data-wiper-button-prev="hero-slider"]',
-            nextEl: '[data-wiper-button-next="hero-slider"]'
-        },
-        on: {
-            init: () => {
-                container.classList.remove('overflow-hidden');
-            }
-        },
-    });
-}
-
-slider();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	
+/******/ 	
+/******/ })()
+;
